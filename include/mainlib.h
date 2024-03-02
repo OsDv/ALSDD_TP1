693083b07bf1104c1ba92defc87e81e06b8b3e3d
Oussama <oussamammm7@gmail.com>
2024-03-02 11:09:02 +0100

    update files and directories

---
+++ include/mainlib.h
@@ -0,0 +1,18 @@
+#ifndef MAINLIB_H_INCLUDED
+#define MAINLIB_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+
+
+
+
+
+
+
+
+#endif // LIB_H_INCLUDED
--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include <mainlib.h>
 int main(){
     printf("Hello world");
+        printf("Hello world");
+
     return 0;
 }
