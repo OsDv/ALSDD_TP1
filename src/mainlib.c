3a2916f1edcec3592c28dc137c90f502955d54a0
Oussama <oussamammm7@gmail.com>
2024-03-08 21:18:51 +0100

    fix error

--- src/mainlib.c
+++ src/mainlib.c
@@ -1,6 +1,11 @@
 #include <mainlib.h>
 
                                     /*  Abstract machine    */
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 void accAllocate(accountP *p){ // allocate a memory for an account node and assigne its adress to "p" 
     *p= (accountP) malloc(80); // size of accountNode = 80 
 }
@@ -18,6 +23,7 @@ void accCFName(accountP p,char *arg_fName){ // Copy the first name of client of 
      while((p->data.customer.fName[i])&&(i<20)){
         arg_fName[i]=p->data.customer.fName[i];
      }
+<<<<<<< Updated upstream
      arg_fName[20]='\0';
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,6 +33,21 @@ void accCLName(accountP p,char *arg_fLame){ // Copy the last name of client of a
         arg_fLame[i]=p->data.customer.lName[i];
      }
      arg_fLame[20]='\0';
+=======
+     arg_fName[i]='\0';
+     arg_fName[20]='\0';// last character always "\0" for protection
+
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+void accCLName(accountP p,char *arg_lName){ // Copy the last name of client of account pointed by "p" in string arg_lName
+    int i=0;
+     while((p->data.customer.lName[i])&&(i<20)){
+        arg_lName[i]=p->data.customer.lName[i];
+        i++;
+     }
+     arg_lName[i]='\0';
+     arg_lName[20]='\0';// last character always "\0" for protection
+>>>>>>> Stashed changes
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
 unsigned long accBalence(accountP p){
@@ -45,6 +66,10 @@ accountP accPrev(accountP p){   // returns the adress of the previus element of 
     return (p->prev);
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 
 
